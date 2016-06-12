2ab7bed605a94e21acb26cfaf2e6edb03cb05742
Alex Gomez <alex.gomez@boltmade.com>
2016-06-12 09:34:41 -0400

    implements make new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,18 @@ class BookingsController < ApplicationController
   end
 
   def create
+    tour_id = params["tour_id"]
+    # TODO: this should really come from session. implement devise.
+    traveller_id = params["traveller_id"]
+
+    query_tour = Tour.find_by(id: tour_id)
+    query_traveller = Traveller.find_by(id: traveller_id)
+    @booking = Booking.create
+    @booking.tour = query_tour
+    @booking.traveller = query_traveller
+    @booking.save
+
+    render json: {success: "booking created successfully"}, status: 200
   end
 
   def show
