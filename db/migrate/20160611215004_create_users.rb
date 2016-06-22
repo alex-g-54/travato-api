c6f479eafff639d8b6c33d5d2c775de45bd63b1a
Jahanzeb Khan <jahanzebkhan@v1020-wn-40-54.campus-dynamic.uwaterloo.ca>
2016-06-22 19:23:15 -0400

    Cleaned up routes.

--- db/migrate/20160611215004_create_users.rb
+++ db/migrate/20160611215004_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :type
       t.string :username
-      t.string :email
+      # t.string :email
       t.string :password
       t.string :full_name
       t.text :description
