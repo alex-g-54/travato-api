9d8a895775e93ab757d49a6a90869e2828e51dd4
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-12 15:36:22 -0400

    flow to edit form works basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   def gate
     if current_user.type.nil?
       # first time logging in
-      render 'new'
+      @user = current_user
+      render 'edit'
     else
       redirect_to page_path('dashboard')
     end
