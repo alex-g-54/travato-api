0667a8aff15d7a0201c172464816a02a455e13c7
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-12 11:51:53 -0400

    implements login with facebook

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'high_voltage', '~> 3.0.0'
+
 gem 'devise'
 gem 'omniauth-facebook'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   post '/fb_login', to: 'users#fb_login', as: 'fb_login'
   post '/register', to: 'users#register', as: 'register'
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   resources :tours
   post '/traveller/:id/tours', to: 'tour#my_tours', as: 'my_tours'
 
-  root to: "tours#index"
+  root to: "pages#welcome"
 end
