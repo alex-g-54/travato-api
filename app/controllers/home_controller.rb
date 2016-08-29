d0563750ded9aba1fd695d2abb58efed9af9183b
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-29 15:55:02 -0400

    removes useless check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     return render 'pages/signed_out' unless user_signed_in?
 
-    @user = User.find(current_user.id) if user_signed_in?
+    @user = User.find(current_user.id)
 
     if @user.has_type?
       flash[:notice] = @user.returning_flash_message
