0d71737dde003a2eefed8cfa0cd35d03175c1a72
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-30 11:14:00 -0400

    adds serializers and annotate

--- app/models/itinerary_user.rb
+++ app/models/itinerary_user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: itinerary_users
+#
+#  id           :integer          not null, primary key
+#  itinerary_id :integer
+#  user_id      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class ItineraryUser < ActiveRecord::Base
   belongs_to :itinerary, inverse_of: :itinerary_users
   belongs_to :guest, foreign_key: "user_id", class_name: "User"
