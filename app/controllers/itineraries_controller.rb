4404a7e0e5657dce4454f23076f6ee85d0a50bcd
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-30 11:58:54 -0400

    lets app respond with html or json depending on request format

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,12 +1,12 @@
 class ItinerariesController < ApplicationController
   def index
-    # city = params["city"]
-    # start_date = Date.parse(params["start_date"])
-    # end_date = Date.parse(params["end_date"])
-
-    # @itineraries = Itinerary.includes(:user).where(city: city, date: start_date..end_date)
+    # TODO: query by city, start_date, end_date
     @itineraries = Itinerary.all
-    render json: @itineraries
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @itineraries }
+    end
   end
 
   def new
@@ -21,19 +21,16 @@ class ItinerariesController < ApplicationController
   def show
     itinerary_id = params["id"]
     @itinerary = Itinerary.where(id: itinerary_id).limit(1).first
-    render json: @itinerary
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @itinerary }
+    end
   end
 
   def update
   end
 
   def destroy
-  end
-
-  def my_itineraries
-    user_id = params["id"]
-    @user = User.find_by(id: user_id)
-    @itineraries = @users.itineraries.includes(:user)
-    render json: {itineraries: @itineraries}, status: 200, include: :user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,29 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.json { render json: @user }
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    redirect_to user_path(current_user)
+    if @user.save
+      redirect_to user_path(current_user)
+    else
+      respond_to do |format|
+        format.html { render 'edit' }
+        format.json { render json: { "ok": "false", "details": "update failed"} }
+      end
+    end
   end
 end
