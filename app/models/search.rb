23f899cce2133b46657805c2d20c57d0cc38d706
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-31 14:38:35 -0400

    implements search page including results

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: searches
+#
+#  id             :integer          not null, primary key
+#  city           :string
+#  arrival_date   :datetime
+#  departure_date :datetime
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Search < ActiveRecord::Base
+  validates :city, :arrival_date, :departure_date, presence: true
+  validate :valid_date_range?
+
   def itineraries
     @itineraries ||= find_itineraries
   end
@@ -6,7 +21,7 @@ class Search < ActiveRecord::Base
 private
 
   def find_itineraries
-    itineraties = Itinerary.order(:updated_at)
+    itineraries = Itinerary.order(:updated_at)
     itineraries = itineraries.where("city like ?", "%#{city}") if city.present?
     itineraries = itineraries.where(date: arrival_date .. departure_date) if dates_range_present?
     itineraries
@@ -14,5 +29,10 @@ private
 
   def dates_range_present?
     arrival_date.present? && departure_date.present?
+  end
+
+  def valid_date_range?
+    return unless dates_range_present?
+    errors.add(:search, "No bookings from future to past") if arrival_date > departure_date
   end
 end
