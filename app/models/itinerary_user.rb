4987e45b0a3c342e772768f118c51cc89d55eadd
Alex Gomez <alejandrogomez54@gmail.com>
2016-08-31 07:54:34 -0400

    rewires models to user and users to make it work nicely with administrate gem

--- app/models/itinerary_user.rb
+++ app/models/itinerary_user.rb
@@ -11,5 +11,5 @@
 
 class ItineraryUser < ActiveRecord::Base
   belongs_to :itinerary, inverse_of: :itinerary_users
-  belongs_to :guest, foreign_key: "user_id", class_name: "User"
+  belongs_to :user
 end
