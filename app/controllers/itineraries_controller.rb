55a7efd95ae2269410857e77d97084e9e02daa33
Jahanzeb Khan <jahanzeb.khan94@gmail.com>
2016-09-01 19:06:35 -0400

    Made html form and set up controller for creating a new itinerary.

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,8 +1,11 @@
 class ItinerariesController < ApplicationController
   def new
+    @itinerary = Itinerary.new
   end
 
   def create
+    binding.pry
+    Itinerary.create!(itinerary_params)
   end
 
   def edit
@@ -19,8 +22,15 @@ class ItinerariesController < ApplicationController
   end
 
   def update
+    itinerary_id = params["id"]
+    @itinerary = Itinerary.where(id: itinerary_id).limit(1).first
+    @itinerary.update_attributes(itinerary_params)
   end
 
   def destroy
+  end
+
+  def itinerary_params
+    params.require(:itinerary).permit(:name, :city, :price, :description, :total_capacity, :pic_url, :start_time, :end_time) # permit all fields you want to allow
   end
 end
